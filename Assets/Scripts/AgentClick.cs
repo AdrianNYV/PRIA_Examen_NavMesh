fa1528cc6b38626c9f4addc83ee576bbd786fcd3
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-06-06 18:52:02 +0200

    Añadido los 2 OffMeshLinks + Agent con Movimiento de Click

--- Assets/Scripts/AgentClick.cs
+++ Assets/Scripts/AgentClick.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Agent : MonoBehaviour {
+public class AgentClick : MonoBehaviour {
     public Transform target;
     private UnityEngine.AI.NavMeshAgent agent;
 
@@ -11,6 +11,12 @@ public class Agent : MonoBehaviour {
     }
 
     void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                    target.position = hit.point;
+                }
+            }
         agent.destination = target.position;
     }
 }
