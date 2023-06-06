3d193f2e724c24428ea0ac43a586df882585a70e
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-06-06 18:37:20 +0200

    Añadido Script al Agent, y creado el Target

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+    public Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    void Start() {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                    target.position = hit.point;
+                }
+            }
+        agent.destination = target.position;
+    }
+}
